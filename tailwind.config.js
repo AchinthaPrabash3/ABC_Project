701e33a90ab88d0f634c51ed1b3abe922203419b
AchinthaPrabash3 <achinthaprabash3@gmail.com>
2024-08-15 21:54:41 +0530

    switched to a new branch and and added dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#292E36",
+        custom1: "#343942",
+        custom2: "#555555",
+        white2: "#FFF8F5",
+        gold: "#E1B168",
+      },
+      fontFamily: {
+        cormorant: ["Cormorant Infant", "sans-serif"],
+        Josefin: ["Josefin Sans", "sans-serif"],
+      },
+      lineHeight: {
+        "extra-tight": "0.7",
+        tighter: "0.8",
+      },
+      backgroundImage: {
+        formbg: "url(./src/assets/formBg.jpg)",
+      },
+    },
+  },
+  plugins: [],
+};
